2ff63155bcf4b7a07bbdf167f727f461c015b193
dan <8881965+dcrgll@users.noreply.github.com>
2022-12-10 10:53:44 +0000

    Add Favicon and OG Image

---
+++ src/app/head.tsx
@@ -0,0 +1,34 @@
+export default function Head() {
+	return (
+		<>
+			<title>Who Owns Staging</title>
+			<meta property="og:title" content="who-owns-staging" />
+			<meta property="og:site_name" content="who-owns-staging" />
+			<meta property="og:url" content="https://who-owns-staging.vercel.app" />
+			<meta property="og:description" content="staging. who owns it?" />
+			<meta property="og:type" content="" />
+			<meta property="og:image" content="/who-owns-staging.png" />
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/apple-touch-icon.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="/favicon-32x32.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="/favicon-16x16.png"
+			/>
+			<link rel="manifest" href="/site.webmanifest" />
+			<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+			<meta name="msapplication-TileColor" content="#da532c" />
+			<meta name="theme-color" content="#ffffff" />
+		</>
+	)
+}
